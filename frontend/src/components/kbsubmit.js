4bc706fb6c7128327b04a466ef63d845054bb29a
Wembley <wembley@amazon.com>
2024-06-24 23:33:29 -0300

    complete commit

---
+++ frontend/src/components/kbsubmit.js
@@ -0,0 +1,75 @@
+import { Fragment, useState } from "react";
+import { Button, Box } from "@mui/material";
+import axios from 'axios';
+import { API_GATEWAY } from '../config';
+
+const PromptSubmit = () => {
+  const [prompt, setField1] = useState('');
+  const [filter, setField2] = useState('');
+  const [data, setData] = useState(null);
+
+
+  const handleSubmit = async () => {
+    try {
+      const params = {
+        prompt: prompt || "undefined",
+        filter: filter || "undefined",
+      };
+      const response = await axios.get(`${API_GATEWAY}/kb`, {params},
+      );
+      console.log(response.data.body);
+      setData(response.data);
+      
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
+  return (
+    <Box>
+        <Fragment>
+          <label>
+            <label htmlFor="textField">Prompt:</label><br></br>
+            <textarea
+              type="text"
+              value={prompt}
+              onChange={(e) => setField1(e.target.value)}
+              rows="5" 
+              cols="33"
+            /><br></br>
+            <label htmlFor="textField">Disciplina: </label>
+            <input
+              type="text"
+              value={filter}
+              onChange={(e) => setField2(e.target.value)}
+              placeholder="Insira a disciplina"
+            /><br></br>
+            <Button
+              variant="contained"
+              component="span"
+              onClick={handleSubmit}
+            >
+              Enviar
+            </Button>
+            {data && (
+              <div>
+                <h2>Resposta:</h2>
+                {data.body.answer.split('\n').map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+                <hr></hr>
+                <h3>Referências:</h3>
+                {data.body.citations.map((citation, index) => (
+                  <div key={index}>
+                    {JSON.stringify(citation.retrievedReferences[index].location.s3Location.uri, null, 2)}
+                  </div>
+                ))}
+              </div>
+            )}
+          </label>
+        </Fragment>
+    </Box>
+  );
+};
+
+export default PromptSubmit;
---
+++ frontend/src/config.js
@@ -0,0 +1,1 @@
+export const API_GATEWAY = "https://rt03q139eb.execute-api.us-east-1.amazonaws.com/prod";
